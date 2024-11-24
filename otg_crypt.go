83e3e20352092eccba977e3c189338d32d1678ad
awangelo <anggg.gaio@gmail.com>
2024-11-23 22:53:40 -0300

    style: capitalized errorf

--- otg_crypt.go
+++ otg_crypt.go
@@ -11,36 +11,41 @@ import (
 func (d Data) encrypt() error {
 	input, err := d.readInputFile()
 	if err != nil {
-		return fmt.Errorf("Error reading input file: %w", err)
+		return fmt.Errorf("error reading input file: %w", err)
 	}
 
 	padLen := len(input) // padLen tem o tamanho do pad que sera usado
-	pad := make([]byte, padLen, padLen)
+	pad := make([]byte, padLen)
 	// verifica se o tamanho do pad esta correto
-	if pad == nil || len(pad) != padLen {
-		return fmt.Errorf("Error allocating pad size: %w", err)
+	if len(pad) != padLen {
+		return fmt.Errorf("error allocating pad size: %w", err)
 	}
 	// pad eh preenchido com valores verdadeiramente aleatorios
 	_, err = rand.Read(pad)
 	if err != nil {
-		return fmt.Errorf("Error creating pad: %w", err)
+		return fmt.Errorf("error creating pad: %w", err)
 	}
 
 	// O arquivo encriptado e criado e preenchido com os dados criptografados
 	encLen := padLen
-	encrypted := make([]byte, encLen, encLen)
+	encrypted := make([]byte, encLen)
 	// verifica se o tamanho do arquivo encriptado esta correto
-	if encrypted == nil || len(encrypted) != encLen {
-		return fmt.Errorf("Error allocating encrypted size: %w", err)
+	if len(encrypted) != encLen {
+		return fmt.Errorf("error allocating encrypted size: %w", err)
 	}
 	// O arquivo eh encriptado usando Vernam cipher (XOR)
 	for i, v := range input {
 		encrypted[i] = v ^ pad[i]
 	}
 
+	// Escreve o arquivo encriptados e o pad
 	err = d.writeEncryptedFile(encrypted)
 	if err != nil {
-		return fmt.Errorf("Error writing encrypted file: %w", err)
+		return fmt.Errorf("error writing encrypted file: %w", err)
+	}
+	err = d.writePadFile(pad)
+	if err != nil {
+		return fmt.Errorf("error writing pad file: %w", err)
 	}
 
 	return nil
@@ -50,8 +55,10 @@ func (d Data) encrypt() error {
 // Este metodo requer uma chave (pad) que seja tao longa (ou maior) quanto os dados criptografados.
 // A funcao le o conteudo do arquivo da chave (pad), realiza a descriptografia e retorna um erro se ocorrer algum problema.
 func (d Data) decrypt() error {
-	pad, err := d.readPadFile()
+	_, err := d.readPadFile()
 	if err != nil {
-		return fmt.Errorf("Error reading pad file: %w", err)
+		return fmt.Errorf("error reading pad file: %w", err)
 	}
+
+	return nil
 }
