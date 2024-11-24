25edb15001c6a9b9c73f9afec9911eacb7cea658
awangelo <anggg.gaio@gmail.com>
2024-11-23 22:53:13 -0300

    style: better error printing

--- one_time_go.go
+++ one_time_go.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var data Data
@@ -11,7 +12,7 @@ func init() {
 	flag.StringVar(&data.InputFile, "input", "", "Input file (required)")
 	flag.StringVar(&data.OutputFile, "output", "encrypted.out", "Encrypted output file name")
 	flag.StringVar(&data.PadFile, "pad", "pad.out", "Pad file name")
-	flag.StringVar(&data.InputFile, "i", "", "Alias for input")
+	flag.StringVar(&data.InputFile, "i", "", "Alias for input (required)")
 	flag.StringVar(&data.OutputFile, "o", "encrypted.out", "Alias for output")
 	flag.StringVar(&data.PadFile, "p", "pad.out", "Alias for pad")
 
@@ -28,13 +29,15 @@ func main() {
 	if data.isEncrypted() {
 		err := data.decrypt()
 		if err != nil {
-			panic(err)
+			fmt.Println("Error decrypting file: ", err)
+			os.Exit(1)
 		}
 		fmt.Println("File decrypted successfully: ", data.OutputFile)
 	} else {
 		err := data.encrypt()
 		if err != nil {
-			panic(err)
+			fmt.Println("Error encrypting file: ", err)
+			os.Exit(1)
 		}
 		fmt.Printf("File encrypted successfully: %s, %s\n", data.OutputFile, data.PadFile)
 	}
